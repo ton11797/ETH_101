312b5c6879801af08af833cd8a3db56e36d27018
Pongsatorn Manusopit <pongsatorn@bluebik.com>
2021-06-04 17:59:07 +0700

    ฺBallot testcase

--- test/sample-test.js
+++ test/sample-test.js
@@ -53,5 +53,61 @@ describe("sample-test",async function() {
     expect(await coin.getBalance(user1)).to.equal(0);
     expect(await coin.getBalance(user2)).to.equal(100);
   });
+  
 });
 
+describe("Ballot",async function() {
+  let Token;
+  let hardhatToken;
+  let owner;
+  let addr1;
+  let addr2;
+  let addr3;
+  let addr4;
+
+  beforeEach(async function () {
+    Ballot = await ethers.getContractFactory("Ballot");
+    [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+    ballot = await Ballot.deploy(["0x7465737400000000000000000000000000000000000000000000000000000000","0x4e69636b00000000000000000000000000000000000000000000000000000000"]);
+    await ballot.deployed();
+  });
+
+  it("giveRightToVote to user 1", async function() {
+    await ballot.giveRightToVote(addr1.address)
+  });
+
+  it("user 1 delegate to user 2", async function() {
+    await ballot.giveRightToVote(addr1.address)
+    await ballot.connect(addr1).delegate(addr2.address)
+    let voterDetail = await ballot.voters(addr1.address)
+    expect(voterDetail.delegate).to.equal(addr2.address)
+  });
+
+  it("vote to proposal 1", async function() {
+    await ballot.giveRightToVote(addr1.address)
+    await ballot.giveRightToVote(addr2.address)
+    let proposal1 = await ballot.proposals(0)
+    expect(proposal1.voteCount).to.equal(0)
+    await ballot.connect(addr1).vote(0)
+    proposal1 = await ballot.proposals(0)
+    expect(proposal1.voteCount).to.equal(1)
+  });
+
+  it("vote to proposal 1 and count vote", async function() {
+    await ballot.giveRightToVote(addr1.address)
+    await ballot.giveRightToVote(addr2.address)
+    await ballot.connect(addr1).vote(0)
+    await ballot.connect(addr2).vote(0)
+    let result = await ballot.winningProposal()
+    expect(result).to.equal(0)
+  });
+
+  it("get winner name", async function() {
+    await ballot.giveRightToVote(addr1.address)
+    await ballot.giveRightToVote(addr2.address)
+    await ballot.connect(addr1).vote(0)
+    await ballot.connect(addr2).vote(0)
+    let result = await ballot.winnerName()
+    expect(result).to.equal("0x7465737400000000000000000000000000000000000000000000000000000000")
+  });
+});
